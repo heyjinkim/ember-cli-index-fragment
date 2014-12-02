f50bf67de519df72f55cd9d006fef9406bcc831f
HeyjinJS <kim.heyjin@gmail.com>
2014-12-02 16:37:36 -0500

    generate fragment-head and fragment-body

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 'use strict';
+var path = require('path');
+var fs = require('fs');
+var chalk = require('chalk');
 
 module.exports = {
-  name: 'lc-addon'
+  name: 'lc-addon',
+  postBuild: function(result){
+    var index = path.join(result.directory, 'index.html');
+    var content = fs.readFileSync(index, {encoding: 'utf8'});
+
+    var startHeader = content.indexOf('<!-- fragment:head -->');
+    var endHeader = content.indexOf('<!-- /fragment:head -->');
+    var pathHeader = path.join(this.project.root, 'dist', 'ember-fragment-head.html');
+    if (startHeader >= 0 && endHeader >= 0){
+      var contentHeader = content.slice(startHeader, endHeader);
+      fs.writeFileSync(pathHeader, contentHeader);
+      console.log(chalk.green('\nGenerated ' + pathHeader  + ' successfully.'));
+    }else{
+      console.log(chalk.red('\nCouldn\'t find ' + pathHeader  + '.'));
+    }
+
+    var startBody = content.indexOf('<!-- fragment:body -->');
+    var endBody = content.indexOf('<!-- /fragment:body -->');
+    var pathBody = path.join(this.project.root, 'dist', 'ember-fragment-body.html');
+    if (startBody >= 0 && endBody >= 0){
+      var contentBody = content.slice(startBody, endBody);
+      fs.writeFileSync(pathBody, contentBody);
+      console.log(chalk.green('Generated ' + pathBody  + ' successfully.'));
+    }else{
+      console.log(chalk.red('Couldn\'t find ' + pathHeader  + '.'));
+    }
+  }
 };
